cd3569f47ec6e28b6c90754a28bc357027871722
jayven <kassarar@gmail.com>
2018-03-08 10:15:54 +0800

    unifiedorder 中 ip 似乎不是必填的

--- mch/unifiedorder.go
+++ mch/unifiedorder.go
@@ -7,43 +7,42 @@ import (
 )
 
 var (
-	ErrUnifiedOrderMissingOutTradeNo     = errors.New("Missing out_trade_no in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingTotalFee       = errors.New("Missing total_fee in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingBody           = errors.New("Missing body in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingSpbillCreateIp = errors.New("Missing spbill_create_ip in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingNotifyUrl      = errors.New("Missing notify_url in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingTradeType      = errors.New("Missing trade_type in UnifiedOrderRequest")
-	ErrUnifiedOrderMissingOpenID         = errors.New("Missing openid in UnifiedOrderRequest since trade_type is JSAPI")
-	ErrUnifiedOrderMissingProductID      = errors.New("Missing product_id in UnifiedOrderRequest since trade_type is NATIVE")
-	ErrUnifiedOrderBadTradeType          = errors.New("Bad trade_type is returned from UnifiedOrderResponse")
-	ErrUnifiedOrderNoPrepayID            = errors.New("No prepay_id is returned from UnifiedOrderResponse")
-	ErrUnifiedOrderNoCodeUrl             = errors.New("No code_url is returned from UnifiedOrderResponse")
-	ErrUnifiedOrderNoMWebUrl             = errors.New("No mweb_url is returned from UnifiedOrderResponse")
+	ErrUnifiedOrderMissingOutTradeNo = errors.New("Missing out_trade_no in UnifiedOrderRequest")
+	ErrUnifiedOrderMissingTotalFee   = errors.New("Missing total_fee in UnifiedOrderRequest")
+	ErrUnifiedOrderMissingBody       = errors.New("Missing body in UnifiedOrderRequest")
+	ErrUnifiedOrderMissingNotifyUrl  = errors.New("Missing notify_url in UnifiedOrderRequest")
+	ErrUnifiedOrderMissingTradeType  = errors.New("Missing trade_type in UnifiedOrderRequest")
+	ErrUnifiedOrderMissingOpenID     = errors.New("Missing openid in UnifiedOrderRequest since trade_type is JSAPI")
+	ErrUnifiedOrderMissingProductID  = errors.New("Missing product_id in UnifiedOrderRequest since trade_type is NATIVE")
+	ErrUnifiedOrderBadTradeType      = errors.New("Bad trade_type is returned from UnifiedOrderResponse")
+	ErrUnifiedOrderNoPrepayID        = errors.New("No prepay_id is returned from UnifiedOrderResponse")
+	ErrUnifiedOrderNoCodeUrl         = errors.New("No code_url is returned from UnifiedOrderResponse")
+	ErrUnifiedOrderNoMWebUrl         = errors.New("No mweb_url is returned from UnifiedOrderResponse")
 )
 
 // UnifiedOrderRequest 为统一下单接口请求
 type UnifiedOrderRequest struct {
 	// ----- 必填字段 -----
-	OutTradeNo     string    // out_trade_no String(32) 商户系统内部订单号 同一个商户号下唯一
-	TotalFee       uint64    // total_fee Int 标价金额 单位为分
-	Body           string    // body String(128) 商品描述 <商场名>-<商品名>
-	SpbillCreateIp string    // spbill_create_ip String(16) 终端IP
-	NotifyUrl      string    // notify_url String(256) 通知地址
-	TradeType      TradeType // trade_type String(16) 交易类型
+	OutTradeNo string    // out_trade_no String(32) 商户系统内部订单号 同一个商户号下唯一
+	TotalFee   uint64    // total_fee Int 标价金额 单位为分
+	Body       string    // body String(128) 商品描述 <商场名>-<商品名>
+	NotifyUrl  string    // notify_url String(256) 通知地址
+	TradeType  TradeType // trade_type String(16) 交易类型
 
 	// ----- 特定条件必填字段 -----
 	OpenID string // openid String(128) 用户标识 trade_type 为 JSAPI 时必填
 
 	// ----- 选填字段 -----
-	ProductID  string    // product_id String(32) 商户自定义商品 ID trade_type 为 NATIVE 时必传
-	DeviceInfo string    // device_info String(32) 设备号
-	Detail     string    // detail String(6000) 商品详情
-	Attach     string    // attach String(127) 附加数据
-	FeeType    string    // fee_type String(16) 标价币种
-	TimeStart  time.Time // time_start String(14) 交易起始时间 格式如 20091225091010
-	TimeExpire time.Time // time_expire String(14) 交易结束时间
-	GoodsTag   string    // goods_tag String(32) 订单优惠标记
-	LimitPay   string    // limit_pay String(32)指定支付方式
+	ProductID      string    // product_id String(32) 商户自定义商品 ID trade_type 为 NATIVE 时必传
+	DeviceInfo     string    // device_info String(32) 设备号
+	SpbillCreateIp string    // spbill_create_ip String(16) 终端IP
+	Detail         string    // detail String(6000) 商品详情
+	Attach         string    // attach String(127) 附加数据
+	FeeType        string    // fee_type String(16) 标价币种
+	TimeStart      time.Time // time_start String(14) 交易起始时间 格式如 20091225091010
+	TimeExpire     time.Time // time_expire String(14) 交易结束时间
+	GoodsTag       string    // goods_tag String(32) 订单优惠标记
+	LimitPay       string    // limit_pay String(32)指定支付方式
 
 	// TOOD: 单品优惠
 }
@@ -88,12 +87,6 @@ func UnifiedOrder(ctx context.Context, config Config, req *UnifiedOrderRequest, 
 		reqXML.fillString(req.Body, "body")
 	}
 
-	if req.SpbillCreateIp == "" {
-		return nil, ErrUnifiedOrderMissingSpbillCreateIp
-	} else {
-		reqXML.fillString(req.SpbillCreateIp, "spbill_create_ip")
-	}
-
 	if req.NotifyUrl == "" {
 		return nil, ErrUnifiedOrderMissingNotifyUrl
 	} else {
@@ -115,6 +108,9 @@ func UnifiedOrder(ctx context.Context, config Config, req *UnifiedOrderRequest, 
 
 	if req.ProductID != "" {
 		reqXML.fillString(req.ProductID, "product_id")
+	}
+	if req.SpbillCreateIp != "" {
+		reqXML.fillString(req.SpbillCreateIp, "spbill_create_ip")
 	}
 	if req.DeviceInfo != "" {
 		reqXML.fillString(req.DeviceInfo, "device_info")
