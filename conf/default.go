c552f36c80cc0c02e83b34d6994126a3241f9147
jayven <kassarar@gmail.com>
2018-03-12 17:46:19 +0800

    修改默认配置的实现

--- conf/default.go
+++ conf/default.go
@@ -1,49 +1,37 @@
 package conf
 
-const (
-	// WechatAppIDName 下存的是微信应用（公众号/小程序...） app id
-	WechatAppIDName = "WechatAppID"
-	// WechatMchIDName 下存的是微信支付商户号
-	WechatMchIDName = "WechatMchID"
-	// WechatMchKeyName 下存的是微信支付密钥
-	WechatMchKeyName = "WechatMchKey"
-)
-
-// DefaultConfig 用 map[string]string 存配置
-type DefaultConfig map[string]string
-
-// WechatAppID 返回微信应用（公众号/小程序...） app id
-func (config DefaultConfig) WechatAppID() string {
-	return config[WechatAppIDName]
+// DefaultConfig 是默认配置，它实现多个子模块的配置接口
+type DefaultConfig struct {
+	// AppID 微信应用（公众号/小程序...） app id
+	AppID string
+	// MchID 微信支付商户号
+	MchID string
+	// MchKey 微信支付密钥
+	MchKey string
 }
 
-// SetWechatAppID 设置微信应用（公众号/小程序...） app id
-func (config DefaultConfig) SetWechatAppID(v string) {
-	config[WechatAppIDName] = v
+var (
+	_ MchConfig         = (*DefaultConfig)(nil)
+	_ MchConfigSelector = (*DefaultConfig)(nil)
+)
+
+// WechatAppID 返回微信应用（公众号/小程序...） app id
+func (config *DefaultConfig) WechatAppID() string {
+	return config.AppID
 }
 
 // WechatMchID 返回微信支付商户号
-func (config DefaultConfig) WechatMchID() string {
-	return config[WechatMchIDName]
-}
-
-// SetWechatMchID 设置微信支付商户号
-func (config DefaultConfig) SetWechatMchID(v string) {
-	config[WechatMchIDName] = v
+func (config *DefaultConfig) WechatMchID() string {
+	return config.MchID
 }
 
 // WechatMchKey 返回微信支付密钥
-func (config DefaultConfig) WechatMchKey() string {
-	return config[WechatMchKeyName]
-}
-
-// SetWechatMchKey 设置微信支付密钥
-func (config DefaultConfig) SetWechatMchKey(v string) {
-	config[WechatMchKeyName] = v
+func (config *DefaultConfig) WechatMchKey() string {
+	return config.MchKey
 }
 
 // SelectMch 实现 MchConfigSelector 接口
-func (config DefaultConfig) SelectMch(appID, mchID string) MchConfig {
+func (config *DefaultConfig) SelectMch(appID, mchID string) MchConfig {
 	if appID == "" || mchID == "" {
 		return nil
 	}
